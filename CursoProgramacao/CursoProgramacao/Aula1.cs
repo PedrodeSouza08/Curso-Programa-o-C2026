7c3bf8a2dabd383cc94123db7ee1b892c5e95429
PedrodeSouza08 <pedrosouzaoli08@gmail.com>
2026-02-21 16:40:26 -0300

    Add files via upload

---
+++ CursoProgramacao/CursoProgramacao/Aula1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoProgramacao
+{
+    public class Aula1
+    {
+        int valor1;
+        int valor2;
+
+        //Método Soma
+        public void Soma()
+        {
+
+
+            Console.WriteLine("Informe o valor 1");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor 2");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Total: {valor1 + valor2}");
+        }
+
+        //Multiplicar
+        public void Multipilcar()
+        {
+
+
+            Console.WriteLine("Informe o valor 1");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor 2");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Total: {valor1 * valor2}");
+        }
+
+        //Subtrair
+        public void Subtrair()
+        {
+
+
+            Console.WriteLine("Informe o valor 1");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor 2");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Total: {valor1 - valor2}");
+        }
+
+        //Divisão
+        public void Dividir()
+        {
+
+
+            Console.WriteLine("Informe o valor 1");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor 2");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Total: {valor1 / valor2}");
+                
+        }
+
+
+
+    }
+}
+        
+         
+
+        
+
---
+++ CursoProgramacao/CursoProgramacao/Aula2.cs
@@ -0,0 +1,15 @@
+﻿namespace CursoProgramacao
+{
+    public class Aula2
+    {
+        public void ValorPar()
+        {
+            int numero = 3;
+            int resultado = 0;
+
+            resultado = numero % 2;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
