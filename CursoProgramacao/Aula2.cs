24af75f4a463c73806c4db5498e2837caeeaa8bd
PedrodeSouza08 <pedrosouzaoli08@gmail.com>
2026-02-28 13:31:58 -0300

    Add files via upload

---
+++ CursoProgramacao/Aula2.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.Design;
+
+namespace CursoProgramacao
+{
+    public class Aula2
+    {
+        public void ValorPar()
+        {
+            Console.WriteLine("Informe um valor:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+                
+        }
+        public void MaiorIdade()
+        {
+            Console.WriteLine("Informe a Idade:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 18 >= 0)
+            {
+                Console.WriteLine("Maior de Idade");
+            }
+            else
+            {
+                Console.WriteLine("Menor de Idade");
+            }
+        }
+        public void Nota()
+        {
+            Console.WriteLine("Informe sua Nota");
+            int nota = int.Parse(Console.ReadLine());
+
+            if (nota >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if (nota >= 5)
+            {
+                Console.WriteLine("Recuperação");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
---
+++ CursoProgramacao/Program.cs
@@ -0,0 +1,18 @@
+﻿using CursoProgramacao;
+using System;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Aula2 aula2 = new Aula2();
+        //aula2.ValorPar();
+        //aula2.MaiorIdade();
+        //aula2.Nota();
+
+        Aula3 aula3 = new Aula3();
+        //aula3.AvaliacaoSwitch();
+        aula3.CategoriaIdade();
+    }
+}
